d58d5bf165b9ed2c7734c33f78462e89f634c290
Maelic <teoneau@gmail.com>
2019-01-29 13:45:17 +0100

    modi

--- ProjetShrek/src/TestsUnitaires/test.java
+++ ProjetShrek/src/TestsUnitaires/test.java
@@ -1,9 +1,14 @@
 package TestsUnitaires;
 
+import Sondage.Sondage;
+import Utilisateurs.Admin;
+
 public class test {
 
 	public static void main(String[] args) {
-
+		Admin adm1 = new Admin("maelic", "neaumael");
+		Sondage sond1 = new Sondage("", "", "", 28/01/2019, "28/01/2019");
+		
 	}
 
 }
